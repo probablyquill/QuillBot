e4e30dab0ab914691d2f24be7f3f82e30d071120
probablyquill <48251719+probablyquill@users.noreply.github.com>
2022-02-21 10:07:18 -0500

    Basic Skeleton
    Bot can connect using auth token.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace QuillBot {
+    class QuillBot {
+        public static Task Main(string[] args) => new QuillBot().MainAsync();
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync() {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            String token = "";
+
+            //Log the bot in
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            //Wait indefinitely
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage message) {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
